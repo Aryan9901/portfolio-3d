a9a012a4fe908490d8cc178f85f3284525ec997a
Aryan9901 <911aaryan@gmail.com>
2024-03-13 16:29:05 +0530

    final version 1 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home, { About, Contact, Projects } from "./pages";
+
+import { Footer, Navbar } from "./components";
+import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
-	return (
-		<main className="bg-slate-300/20 h-full">
-			<Router>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route
-						path="/*"
-						element={
-							<>
-								<Routes>
-									<Route path="/about" element={<About />} />
-									<Route path="/projects" element={<Projects />} />
-									<Route path="/contact" element={<Contact />} />
-								</Routes>
-								{/* <Footer /> */}
-							</>
-						}
-					/>
-				</Routes>
-			</Router>
-		</main>
-	);
+  return (
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/*'
+            element={
+              <>
+                <Routes>
+                  <Route path='/about' element={<About />} />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </main>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
