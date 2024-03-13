f1fb59754bd477a1feabf6e75c471734aede86c2
Aryan9901 <911aaryan@gmail.com>
2024-03-13 16:24:26 +0530

    added models

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,25 +1,31 @@
-import { useAnimations, useGLTF } from "@react-three/drei";
-import planeObj from "../assets/3d/plane.glb";
 import { useEffect, useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
 
-const Plane = ({ isRotating, ...props }) => {
-	const ref = useRef();
-	const { scene, animations } = useGLTF(planeObj);
-	const { actions } = useAnimations(animations, ref);
+import planeScene from "../assets/3d/plane.glb";
 
-	useEffect(() => {
-		if (isRotating) {
-			actions["Take 001"].play();
-		} else {
-			actions["Take 001"].stop();
-		}
-	}, [actions, isRotating]);
+// 3D Model from: https://sketchfab.com/3d-models/stylized-ww1-plane-c4edeb0e410f46e8a4db320879f0a1db
+export function Plane({ isRotating, ...props }) {
+  const ref = useRef();
+  // Load the 3D model and its animations
+  const { scene, animations } = useGLTF(planeScene);
+  // Get animation actions associated with the plane
+  const { actions } = useAnimations(animations, ref);
 
-	return (
-		<mesh {...props} ref={ref}>
-			<primitive object={scene} />
-		</mesh>
-	);
-};
+  // Use an effect to control the plane's animation based on 'isRotating'
+  // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+  }, [actions, isRotating]);
 
-export default Plane;
+  return (
+    <mesh {...props} ref={ref}>
+      // use the primitive element when you want to directly embed a complex 3D
+      model or scene
+      <primitive object={scene} />
+    </mesh>
+  );
+}
