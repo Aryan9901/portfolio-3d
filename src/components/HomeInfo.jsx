1469e1364c64c9a3819c07019a71dfb4eac9e6ab
Aryan9901 <911aaryan@gmail.com>
2024-03-13 16:25:05 +0530

    added components

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,32 +1,65 @@
 import { Link } from "react-router-dom";
+
 import { arrow } from "../assets/icons";
 
-const InfoBox = ({ text, link, btnText }) => (
-	<div className="info-box">
-		<p className="font-medium sm:text-xl text-center">{text}</p>
-		<Link to={link} className="neo-brutalism-white neo-btn w-52">
-			{btnText}
-			<img src={arrow} alt="arrow" className="w-4 h-4 object-contain" />
-		</Link>
-	</div>
-);
-
-const renderContent = {
-	1: (
-		<h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
-			Hi, I&apos;m
-			<span className="font-semibold mx-2 text-white">Adrian</span>
-			👋
-			<br />A Software Engineer from Croatia
-		</h1>
-	),
-	2: <InfoBox text="Full Stack Developer" link="/about" btnText="Learn More" />,
-	3: <InfoBox text={`Led multiple projects to success over the years. Curious about the impact?`} link="/projects" btnText="Visit my portfolio" />,
-	4: <InfoBox text={"Need a project done or looking for a dev? Im just a few keystrokes away"} link="/contact" btnText="Let's talk" />,
-};
-
 const HomeInfo = ({ currentStage }) => {
-	return renderContent[currentStage] || null;
+  if (currentStage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm
+        <span className='font-semibold mx-2 text-white'>Adrian</span>
+        👋
+        <br />
+        A Software Engineer from Croatia 🇭🇷
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+          Worked with many companies <br /> and picked up many skills along the way
+        </p>
+
+        <Link to='/about' className='neo-brutalism-white neo-btn'>
+          Learn more
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium text-center sm:text-xl'>
+          Led multiple projects to success over the years. <br /> Curious about the impact?
+        </p>
+
+        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+          Visit my portfolio
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>
+        Need a project done or looking for a dev? <br/> I'm just a few keystrokes away
+      </p>
+
+      <Link to='/contact' className='neo-brutalism-white neo-btn'>
+        Let's talk
+        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+      </Link>
+    </div>
+    );
+  }
+
+  return null;
 };
 
 export default HomeInfo;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
 import { NavLink } from "react-router-dom";
 
+import { logo } from "../assets/images";
+
 const Navbar = () => {
-	return (
-		<header className="header">
-			<NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-				<p className="blue-gradient_text">AG</p>
-			</NavLink>
-			<nav className="flex text-lg gap-7 font-medium">
-				<NavLink to="/about" className={({ isActive }) => (isActive ? "text-blue-500" : "text-black")}>
-					About
-				</NavLink>
-				<NavLink to="/projects" className={({ isActive }) => (isActive ? "text-blue-500" : "text-black")}>
-					Projects
-				</NavLink>
-			</nav>
-		</header>
-	);
+  return (
+    <header className='header'>
+      <NavLink to='/'>
+        <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
+      </NavLink>
+      <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          About
+        </NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
