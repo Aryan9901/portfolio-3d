7088d9aa65dde115eacf93081ebc2e3a93d6ac5f
Aryan9901 <911aaryan@gmail.com>
2024-05-05 17:57:47 +0530

    added images imports

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,15 +1,8 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
-import logo from './logo.svg'
+import hero from "./hero.jpg";
+import meta from "./meta.png";
+import shopify from "./shopify.png";
+import starbucks from "./starbucks.png";
+import tesla from "./tesla.png";
+import logo from "./logo.svg";
 
-export {
-    hero,
-    meta,
-    shopify,
-    starbucks,
-    tesla,
-    logo
-}
+export { hero, meta, shopify, starbucks, tesla, logo };
