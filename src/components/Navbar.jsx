98716b7a8e911f78a39e9511719c8689cb8dbacd
Aryan9901 <911aaryan@gmail.com>
2024-05-05 17:58:37 +0530

    changes nav logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 import { NavLink } from "react-router-dom";
 
+// import logo from "../assets/images/logo.png";
 import { logo } from "../assets/images";
 
 const Navbar = () => {
-  return (
-    <header className='header'>
-      <NavLink to='/'>
-        <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
-      </NavLink>
-      <nav className='flex text-lg gap-7 font-medium'>
-        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
-          About
-        </NavLink>
-        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
-          Projects
-        </NavLink>
-      </nav>
-    </header>
-  );
+	return (
+		<header className="header">
+			<NavLink to="/" className="p-4 bg-white rounded-md">
+				<img src={logo} alt="logo" className="w-20 object-contain" />
+			</NavLink>
+			<nav className="flex text-lg gap-7 font-medium">
+				<NavLink to="/about" className={({ isActive }) => (isActive ? "text-blue-600" : "text-black")}>
+					About
+				</NavLink>
+				<NavLink to="/projects" className={({ isActive }) => (isActive ? "text-blue-600" : "text-black")}>
+					Projects
+				</NavLink>
+			</nav>
+		</header>
+	);
 };
 
 export default Navbar;
