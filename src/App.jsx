f9f2111adbd71dd85c32b2e321a85900957d3831
Aryan9901 <911aaryan@gmail.com>
2024-05-05 17:56:38 +0530

    changes some router code

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,29 @@ import { Footer, Navbar } from "./components";
 import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
-  return (
-    <main className='bg-slate-300/20'>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route
-            path='/*'
-            element={
-              <>
-                <Routes>
-                  <Route path='/about' element={<About />} />
-                  <Route path='/projects' element={<Projects />} />
-                  <Route path='/contact' element={<Contact />} />
-                </Routes>
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
-      </Router>
-    </main>
-  );
+	return (
+		<main className="bg-slate-300/20">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route
+						path="/*"
+						element={
+							<>
+								<Routes>
+									<Route path="/about" element={<About />} />
+									<Route path="/projects" element={<Projects />} />
+									<Route path="/contact" element={<Contact />} />
+								</Routes>
+								<Footer />
+							</>
+						}
+					/>
+				</Routes>
+			</Router>
+		</main>
+	);
 };
 
 export default App;
